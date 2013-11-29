b2f6d55eb5daff05905773f6c6df3e2ee1fba6f0
Mark Vasilkov <mvasilkov@gmail.com>
2013-11-29 23:47:07 +0200

    Create a new repository

---
+++ test/basic.js
@@ -0,0 +1,27 @@
+var assert = require('assert'),
+    wto = require('../wto')
+
+describe('što', function () {
+    function eq(a, b) { assert.strictEqual(a, b) }
+    function one(a, b) { assert.notEqual(b.indexOf(a), -1) }
+
+    it('should retain plain text', function () {
+        eq(wto('')(), '')
+        eq(wto('{{mustache}}')(), '{{mustache}}')
+        eq(wto('#define foo bar')(), '#define foo bar')
+    })
+
+    it('should retain trivial groups', function () {
+        eq(wto('#{ab}')(), 'ab')
+        eq(wto('#define #{foo} #{bar}')(), '#define foo bar')
+    })
+
+    it('should select random bits', function () {
+        one(wto('#{a#b}')(), 'ab'.split(''))
+        one(wto('/* #{foo#wto} #{bar#što} */')(),
+            ['/* foo bar */', '/* foo što */', '/* wto bar */', '/* wto što */'])
+        /* nested */
+        one(wto('#{#{a}##{b}}')(), 'ab'.split(''))
+        one(wto('del #{foo##{bar#što}}')(), ['del foo', 'del bar', 'del što'])
+    })
+})
